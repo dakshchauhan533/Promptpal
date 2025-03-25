ff8f4f4b3e941a55057f8ef191cb243ce91804c7
daksh chauhan <chauhandaksh60@gmail.com>
2025-03-25 11:45:02 +0530

    commit

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -23,7 +23,7 @@ const Main = () => {
   return (
     <div className='main'>
       <div className="nav">
-        <p>Gemini</p>
+        <p>Promptpal</p>
         <img src={assets.user_icon} alt="icon" />
       </div>
       <div className="main-container">
